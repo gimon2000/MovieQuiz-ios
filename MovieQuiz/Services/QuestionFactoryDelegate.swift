c1816114acc7ab83c96859d248324125503316b4
Gimon Anton <Gimon2000@yandex.ru>
2024-01-14 22:37:33 +0300

    реализован делегат для фабрики вопросов

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by gimon on 12.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by gimon on 11.01.2024.
+//
+
+import Foundation
