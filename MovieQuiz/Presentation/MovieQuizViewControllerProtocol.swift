b451bc4eb3e76e0f1a27e681082ec855177acf09
Gimon Anton <Gimon2000@yandex.ru>
2024-02-18 20:15:25 +0300

    написан тест на пресентер

---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by gimon on 18.02.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/MovieQuizViewControllerMock.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerMock.swift
+//  MovieQuizTests
+//
+//  Created by gimon on 18.02.2024.
+//
+
+import Foundation
